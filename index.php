021a15cd33770417b9c5973fecff800011e60b4a
shelby <185173274@qq.com>
2019-02-15 14:00:35 +0800

    首页文件

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-	echo'现在的时间:'. data('Y-m-d H:i:S');
+/**
+ * Created by PhpStorm.
+ * User: shelby
+ * Date: 2019/2/15
+ * Time: 13:44
+ * keep going m8 :)
+ */
+header('Content-type:text/html;charset=utf8');
+echo'现在的时间:'. data('Y-m-d H:i:S');
+
+
+
 ?>
